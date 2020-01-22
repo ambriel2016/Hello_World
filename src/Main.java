6d728580a86113cb69778920a96ce3e8b2e0bc19
ambriel2016 <crb3305@gmail.com>
2020-01-22 11:17:54 -0500

    My first project adding a new sentence

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Today is Jan 22, 2020. I am alive!");
+        System.out.println("My name is Cristian!");
         // single comment
         /*
         block comment
